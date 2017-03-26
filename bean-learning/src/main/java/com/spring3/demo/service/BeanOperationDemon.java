0d21ba46980ab5f09519959b8e7cfdb518cf3d15
taoyong <taoyong@u51.com>
2017-03-26 14:24:58 +0800

    Initial commit

---
+++ bean-learning/src/main/java/com/spring3/demo/service/BeanOperationDemon.java
@@ -0,0 +1,10 @@
+package com.spring3.demo.service;
+
+/**
+ * Created with:IntelliJ IDEA.
+ * Created by: taoyong
+ * Time: 2017-03-26 13:10
+ * Description:
+ */
+public class BeanOperationDemon {
+}
